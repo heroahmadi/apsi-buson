dbba54e3fbc8d5f6af95e91377b55d874a252c79
Akbar Ahmadi <hero.lionel@gmail.com>
2017-12-13 17:13:18 +0700

    done templating

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+	@include('includes.head')
+
+	@section('includes-css')
+		<link rel="stylesheet" type="text/css" href="{{ url('source/bootstrap-3.3.6-dist/css/bootstrap.css')}}">
+		<link rel="stylesheet" type="text/css" href="{{ url('source/font-awesome-4.5.0/css/font-awesome.css')}}">
+	@show
+</head>
+<body>
+<!-- Header -->
+<div class="allcontain">
+	@include('includes.topnavbar')
+</div>
+<!-- Content -->
+
+@yield('content')
+
+<div class="allcontain">
+	@include('includes.footer')
+</div>
+
+	@section('includes-scripts')
+		<script type="text/javascript" src="{{ url('source/bootstrap-3.3.6-dist/js/jquery.js')}}"></script>
+		<script type="text/javascript" src="{{ url('source/bootstrap-3.3.6-dist/js/jquery.1.11.js')}}"></script>
+		<script type="text/javascript" src="{{ url('source/bootstrap-3.3.6-dist/js/bootstrap.js')}}"></script>
+	@show
+</body>
+</html>
