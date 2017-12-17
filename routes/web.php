063dba12f0dde629f54de2b1f13b19ebf30ef9f2
Akbar Ahmadi <hero.lionel@gmail.com>
2017-12-17 22:25:25 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 Route::get('/logout', 'Auth\LoginController@logout');
+Route::get('/admin', 'AdminController@index');
 
 Route::get('/home', 'HomeController@index')->name('home');
